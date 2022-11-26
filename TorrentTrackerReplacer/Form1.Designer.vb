6354faa959f7d5b0147c43c9ced7bbbea353870a
zhaoyangwx <32697586+zhaoyangwx@users.noreply.github.com>
2022-11-26 20:35:58 +0800

    Add files via upload

---
+++ TorrentTrackerReplacer/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBoxInfo = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "搜索目录"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(71, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(741, 21)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Y:\qBittorrent\profile\qBittorrent\data\BT_backup"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(818, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(71, 40)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(741, 21)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.Text = "*.torrent"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "匹配文件"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(71, 67)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(350, 21)
+        Me.TextBox3.TabIndex = 5
+        Me.TextBox3.Text = ".ru"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 70)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "替换文本"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(427, 70)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(17, 12)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "->"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox4.Location = New System.Drawing.Point(450, 67)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(362, 21)
+        Me.TextBox4.TabIndex = 8
+        Me.TextBox4.Text = ".wiki"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(818, 70)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Text = "备份"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBoxInfo
+        '
+        Me.TextBoxInfo.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxInfo.Location = New System.Drawing.Point(12, 94)
+        Me.TextBoxInfo.Multiline = True
+        Me.TextBoxInfo.Name = "TextBoxInfo"
+        Me.TextBoxInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxInfo.Size = New System.Drawing.Size(860, 407)
+        Me.TextBoxInfo.TabIndex = 10
+        Me.TextBoxInfo.WordWrap = False
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button2.Location = New System.Drawing.Point(405, 507)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "运行"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(818, 45)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(60, 16)
+        Me.CheckBox2.TabIndex = 12
+        Me.CheckBox2.Text = "子目录"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(884, 542)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBoxInfo)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "TorrentTrackerReplacer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBoxInfo As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents CheckBox2 As CheckBox
+End Class
---
+++ TorrentTrackerReplacer/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports BencodeNET.Parsing
+Imports BencodeNET.Torrents
+
+Public Class Form1
+    Public Sub Info(s As String)
+        TextBoxInfo.AppendText(Now.ToString & "> " & s & vbCrLf)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Not My.Computer.FileSystem.DirectoryExists(TextBox1.Text) Then
+            Info("目录不存在：" & TextBox1.Text)
+            Exit Sub
+        End If
+        Dim so As IO.SearchOption
+        If CheckBox2.Checked Then
+            so = IO.SearchOption.AllDirectories
+        Else
+            so = IO.SearchOption.TopDirectoryOnly
+        End If
+        Dim flist() As IO.FileInfo = My.Computer.FileSystem.GetDirectoryInfo(TextBox1.Text).GetFiles(TextBox2.Text, so)
+        Info("已找到 " & flist.Length & " 个文件")
+        Dim i As Integer = 0
+        Dim tnow As String = Now.ToString("yyyyMMdd_HHmmss")
+        For Each f As IO.FileInfo In flist
+            Threading.Interlocked.Increment(i)
+            Try
+                Info("[" & i & "/" & flist.Length & "]" & f.FullName)
+                Dim t As Torrent
+                t = New BencodeParser(System.Text.Encoding.UTF8).Parse(Of Torrent)(f.FullName)
+                If t.Pieces Is Nothing Then Throw New Exception("文件无效")
+                If t.Pieces.Length = 0 Then Throw New Exception("文件无效")
+                If t.Pieces.Length Mod 20 <> 0 Then Throw New Exception("文件无效")
+                For Each k As IList(Of String) In t.Trackers
+                    For j As Integer = 0 To k.Count - 1
+                        k(j) = k(j).Replace(TextBox3.Text, TextBox4.Text)
+                        ' k(j) = k(j)
+                    Next
+                Next
+                If CheckBox1.Checked Then
+                    Info("保存备份文件：" & f.FullName & "." & tnow & ".bak")
+                    My.Computer.FileSystem.CopyFile(f.FullName, f.FullName & "." & tnow & ".bak")
+                End If
+                Dim s As New IO.FileStream(f.FullName, IO.FileMode.Create)
+                t.EncodeTo(s)
+                s.Close()
+                Info("OK")
+            Catch ex As Exception
+                Info(ex.ToString)
+            End Try
+        Next
+        Info("运行结束")
+    End Sub
+End Class
